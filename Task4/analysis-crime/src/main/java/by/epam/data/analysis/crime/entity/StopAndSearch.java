b5f0b5af192323acea2e9de612df4db3cde6ca19
ant4ded <ant4ded@gmail.com>
2021-02-04 01:31:53 +0300

    Rewrite entities

--- Task4/analysis-crime/src/main/java/by/epam/data/analysis/crime/entity/StopAndSearch.java
+++ Task4/analysis-crime/src/main/java/by/epam/data/analysis/crime/entity/StopAndSearch.java
@@ -4,45 +4,48 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 import java.util.UUID;
 
 @Data
 @Entity
-@Table(name = "stop_and_search")
+@Table
 public class StopAndSearch {
     @Id
     private UUID id = UUID.randomUUID();
     @JsonProperty("age_range")
-    @Column(name = "age_range")
+    @Column
     private String ageRange;
     @JsonProperty("involved_person")
-    @Column(name = "involved_person")
+    @Column
     private boolean involvedPerson;
     @JsonProperty("self_defined_ethnicity")
-    @Column(name = "self_defined_ethnicity")
+    @Column
     private String selfDefinedEthnicity;
+    @Column
+    private String gender;
     @Column
     private String legislation;
     @JsonProperty("outcome_linked_to_object_of_search")
-    @Column(name = "outcome_linked_to_object_of_search")
+    @Column
     private boolean outcomeLinkedToObjectOfSearch;
     @Column
-    private String datetime;
+    private Timestamp datetime;
     @JsonProperty("removal_of_more_than_outer_clothing")
-    @Column(name = "removal_of_more_than_outer_clothing")
+    @Column
     private boolean removalOfMoreThanOuterClothing;
     @Column
-    private String operator;
+    private String operation;
     @JsonProperty("officer_defined_ethnicity")
-    @Column(name = "officer_defined_ethnicity")
+    @Column
     private String officerDefinedEthnicity;
     @Column
     private String type;
     @JsonProperty("operation_name")
-    @Column(name = "operation_name")
+    @Column
     private String operationName;
     @JsonProperty("object_of_search")
-    @Column(name = "object_of_search")
+    @Column
     private String objectOfSearch;
 
     @JsonProperty("outcome_object")
