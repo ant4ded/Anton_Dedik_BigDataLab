a9d87e1a4889cb143b2ff32a09d257a3fa472214
ant4ded <ant4ded@gmail.com>
2021-02-04 01:32:20 +0300

    Added new queries and fix old

--- Task4/1_most_dangerous_streets.sql
+++ Task4/1_most_dangerous_streets.sql
@@ -8,11 +8,11 @@ SELECT DISTINCT
 			CAST(:start AS varchar),
 			' till ',
 			CAST(:end AS varchar)
-		) 		AS Period
+		) 								 AS Period
 FROM prod.street AS s
 INNER JOIN prod.location 	AS l
 	ON l.street_id = s.id
-INNER JOIN prod.crime 	AS c
+INNER JOIN prod.crime 		AS c
 	ON c.location_id = l.id
 WHERE c.month BETWEEN CAST(:start AS varchar) AND  CAST(:end AS varchar)
 ORDER BY crime_count DESC;
--- Task4/db.sql
+++ Task4/db.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 10.15
 -- Dumped by pg_dump version 10.15
 
--- Started on 2021-02-01 00:57:51
+-- Started on 2021-02-03 21:25:37
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -17,12 +17,6 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
-DROP DATABASE uk_police;
---
--- TOC entry 2839 (class 1262 OID 16393)
--- Name: uk_police; Type: DATABASE; Schema: -; Owner: postgres
---
 
 CREATE DATABASE uk_police WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252';
 
@@ -74,7 +68,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 198 (class 1259 OID 21961)
+-- TOC entry 198 (class 1259 OID 22812)
 -- Name: crime; Type: TABLE; Schema: prod; Owner: postgres
 --
 
@@ -94,7 +88,7 @@ CREATE TABLE prod.crime (
 ALTER TABLE prod.crime OWNER TO postgres;
 
 --
--- TOC entry 197 (class 1259 OID 21959)
+-- TOC entry 197 (class 1259 OID 22810)
 -- Name: hibernate_sequence; Type: SEQUENCE; Schema: prod; Owner: postgres
 --
 
@@ -109,7 +103,7 @@ CREATE SEQUENCE prod.hibernate_sequence
 ALTER TABLE prod.hibernate_sequence OWNER TO postgres;
 
 --
--- TOC entry 199 (class 1259 OID 21969)
+-- TOC entry 199 (class 1259 OID 22820)
 -- Name: location; Type: TABLE; Schema: prod; Owner: postgres
 --
 
@@ -124,7 +118,7 @@ CREATE TABLE prod.location (
 ALTER TABLE prod.location OWNER TO postgres;
 
 --
--- TOC entry 200 (class 1259 OID 21974)
+-- TOC entry 200 (class 1259 OID 22825)
 -- Name: outcome_object; Type: TABLE; Schema: prod; Owner: postgres
 --
 
@@ -137,7 +131,7 @@ CREATE TABLE prod.outcome_object (
 ALTER TABLE prod.outcome_object OWNER TO postgres;
 
 --
--- TOC entry 201 (class 1259 OID 21982)
+-- TOC entry 201 (class 1259 OID 22833)
 -- Name: outcome_status; Type: TABLE; Schema: prod; Owner: postgres
 --
 
@@ -151,20 +145,21 @@ CREATE TABLE prod.outcome_status (
 ALTER TABLE prod.outcome_status OWNER TO postgres;
 
 --
--- TOC entry 202 (class 1259 OID 21990)
+-- TOC entry 202 (class 1259 OID 22841)
 -- Name: stop_and_search; Type: TABLE; Schema: prod; Owner: postgres
 --
 
 CREATE TABLE prod.stop_and_search (
     id uuid NOT NULL,
     age_range character varying(255),
-    datetime character varying(255),
+    datetime timestamp without time zone,
+    gender character varying(255),
     involved_person boolean,
     legislation character varying(255),
     object_of_search character varying(255),
     officer_defined_ethnicity character varying(255),
+    operation character varying(255),
     operation_name character varying(255),
-    operator character varying(255),
     outcome_linked_to_object_of_search boolean,
     removal_of_more_than_outer_clothing boolean,
     self_defined_ethnicity character varying(255),
@@ -192,7 +187,7 @@ CREATE SEQUENCE prod.stop_and_search_sequence
 ALTER TABLE prod.stop_and_search_sequence OWNER TO postgres;
 
 --
--- TOC entry 203 (class 1259 OID 21998)
+-- TOC entry 203 (class 1259 OID 22849)
 -- Name: street; Type: TABLE; Schema: prod; Owner: postgres
 --
 
@@ -205,7 +200,7 @@ CREATE TABLE prod.street (
 ALTER TABLE prod.street OWNER TO postgres;
 
 --
--- TOC entry 2697 (class 2606 OID 21968)
+-- TOC entry 2697 (class 2606 OID 22819)
 -- Name: crime crime_pkey; Type: CONSTRAINT; Schema: prod; Owner: postgres
 --
 
@@ -214,7 +209,7 @@ ALTER TABLE ONLY prod.crime
 
 
 --
--- TOC entry 2699 (class 2606 OID 21973)
+-- TOC entry 2699 (class 2606 OID 22824)
 -- Name: location location_pkey; Type: CONSTRAINT; Schema: prod; Owner: postgres
 --
 
@@ -223,7 +218,7 @@ ALTER TABLE ONLY prod.location
 
 
 --
--- TOC entry 2701 (class 2606 OID 21981)
+-- TOC entry 2701 (class 2606 OID 22832)
 -- Name: outcome_object outcome_object_pkey; Type: CONSTRAINT; Schema: prod; Owner: postgres
 --
 
@@ -232,7 +227,7 @@ ALTER TABLE ONLY prod.outcome_object
 
 
 --
--- TOC entry 2703 (class 2606 OID 21989)
+-- TOC entry 2703 (class 2606 OID 22840)
 -- Name: outcome_status outcome_status_pkey; Type: CONSTRAINT; Schema: prod; Owner: postgres
 --
 
@@ -241,7 +236,7 @@ ALTER TABLE ONLY prod.outcome_status
 
 
 --
--- TOC entry 2705 (class 2606 OID 21997)
+-- TOC entry 2705 (class 2606 OID 22848)
 -- Name: stop_and_search stop_and_search_pkey; Type: CONSTRAINT; Schema: prod; Owner: postgres
 --
 
@@ -250,7 +245,7 @@ ALTER TABLE ONLY prod.stop_and_search
 
 
 --
--- TOC entry 2707 (class 2606 OID 22002)
+-- TOC entry 2707 (class 2606 OID 22853)
 -- Name: street street_pkey; Type: CONSTRAINT; Schema: prod; Owner: postgres
 --
 
@@ -259,7 +254,7 @@ ALTER TABLE ONLY prod.street
 
 
 --
--- TOC entry 2708 (class 2606 OID 22003)
+-- TOC entry 2708 (class 2606 OID 22854)
 -- Name: crime fk3xkq94wviwgirfepd8dxrwum6; Type: FK CONSTRAINT; Schema: prod; Owner: postgres
 --
 
@@ -268,7 +263,7 @@ ALTER TABLE ONLY prod.crime
 
 
 --
--- TOC entry 2710 (class 2606 OID 22013)
+-- TOC entry 2710 (class 2606 OID 22864)
 -- Name: location fk57sm5p4awhw8awu1rkmk5lt5q; Type: FK CONSTRAINT; Schema: prod; Owner: postgres
 --
 
@@ -277,7 +272,7 @@ ALTER TABLE ONLY prod.location
 
 
 --
--- TOC entry 2712 (class 2606 OID 22023)
+-- TOC entry 2712 (class 2606 OID 22874)
 -- Name: stop_and_search fkb7u7sp0r25lyu3n2i1iqiv8k2; Type: FK CONSTRAINT; Schema: prod; Owner: postgres
 --
 
@@ -286,7 +281,7 @@ ALTER TABLE ONLY prod.stop_and_search
 
 
 --
--- TOC entry 2709 (class 2606 OID 22008)
+-- TOC entry 2709 (class 2606 OID 22859)
 -- Name: crime fkhvyimqb89v8x3678c9556yjxf; Type: FK CONSTRAINT; Schema: prod; Owner: postgres
 --
 
@@ -295,7 +290,7 @@ ALTER TABLE ONLY prod.crime
 
 
 --
--- TOC entry 2711 (class 2606 OID 22018)
+-- TOC entry 2711 (class 2606 OID 22869)
 -- Name: stop_and_search fktdad06k2j0729mo1ykm0y99gx; Type: FK CONSTRAINT; Schema: prod; Owner: postgres
 --
 
@@ -303,7 +298,7 @@ ALTER TABLE ONLY prod.stop_and_search
     ADD CONSTRAINT fktdad06k2j0729mo1ykm0y99gx FOREIGN KEY (location_id) REFERENCES prod.location(id);
 
 
--- Completed on 2021-02-01 00:57:51
+-- Completed on 2021-02-03 21:25:37
 
 --
 -- PostgreSQL database dump complete
