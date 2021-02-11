2544ded58dd5909086471a3a21a654d55b66b7cd
ant4ded <ant4ded@gmail.com>
2021-02-11 16:07:43 +0300

    Added jenkins job configuration

--- Task5/analysis-crime/src/main/java/by/epam/data/analysis/crime/repository/StopAndSearchRepository.java
+++
@@ -1,9 +0,0 @@
-package by.epam.data.analysis.crime.repository;
-
-import by.epam.data.analysis.crime.entity.StopAndSearch;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StopAndSearchRepository extends JpaRepository<StopAndSearch, Long> {
-}
